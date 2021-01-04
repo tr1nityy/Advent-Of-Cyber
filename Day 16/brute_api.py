74ec03d55e8ae5a0c80522b07e46355425f1f4f3
tr1nityy <71458329+tr1nityy@users.noreply.github.com>
2021-01-04 10:38:34 +0100

    Identation fix

--- Day 16/brute_api.py
+++ Day 16/brute_api.py
@@ -9,11 +9,11 @@ for i in range(0, 101):
   	# We're looking for odd numbers
 	if (i % 2 != 0):
     	# Send a request to TARGET appending the key to it (odd number)
-		response = requests.get(TARGET + str(i))
+	    response = requests.get(TARGET + str(i))
 
-    	# Filter specific string in the response from the actual response (location)
-		if 'PROTECTION' not in response.text and response.status_code == 200:
-			print('API key:', str(i))
-			print(response.text)
-			break
+            # Filter specific string in the response from the actual response (location)
+	    if 'PROTECTION' not in response.text and response.status_code == 200:
+		print('API key:', str(i))
+ 	        print(response.text)
+		break
 
